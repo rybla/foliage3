9bb6a45556b74b4c363dc74d2a8b9fac20ba25f6
rybla <henry@monkeyface.com>
2025-02-11 10:50:34 -0500

    [checkpoint]

--- src/Foliage/Example/Ex1.purs
+++ src/Foliage/Example/Ex1.purs
@@ -9,14 +9,14 @@ prog :: Prog
 prog =
   Prog $ List.fromFoldable
     [ DefRel relP NatLat
+    , DefRule (Name "R2") $ Rule
+        { hyps: List.fromFoldable []
+        , prop: Prop (Rel relP) $ DataTerm (NatTerm 0)
+        }
     , DefRule (Name "R1") $ Rule
         { hyps: List.fromFoldable
             [ PropHyp $ Prop (Rel relP) $ DataTerm (NatTerm 0) ]
         , prop: Prop (Rel relP) $ DataTerm (NatTerm 1)
-        }
-    , DefRule (Name "R2") $ Rule
-        { hyps: List.fromFoldable []
-        , prop: Prop (Rel relP) $ DataTerm (NatTerm 0)
         }
     ]
   where
